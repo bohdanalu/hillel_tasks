4b86d7ebb00efc96853a2086167c020b34725943
bohdanalu <b_dana@ukr.net>
2024-01-24 10:50:49 +0200

    add components in task_1

---
+++ task_1/src/components/Input/Input.jsx
@@ -0,0 +1,10 @@
+const Input = () => {
+  return (
+    <>
+      <label htmlFor="pswd">Enter your password</label>
+      <input type="password" required id="pswd" />
+    </>
+  );
+};
+
+export default Input;
